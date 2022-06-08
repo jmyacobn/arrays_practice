17bdf5832725c37fbdf63201ed5e3490fde1e340
Jenn Yacoubian <jyacoubian@gmail.com>
2022-06-08 14:26:55 -0600

    Call array methods

--- arrays.js
+++ arrays.js
@@ -1,8 +1,24 @@
-//#Array of Strings
+//Array of Strings
 var friends = ["Karin", "Katie", "Rachel", "Camay", "Brandon"]
+console.log(friends);
 
-//#Array of Numbers
+//I am using the pop method to remove the last entry in my array.
+//Line 9 will call out the last item.
+//Line 10 will list the array without the last element.
+console.log(friends.pop());
+console.log(friends);
+
+//Array of Numbers
 var ages = [12, 13, 14, 15, 16]
+console.log(ages);
 
-//#Array of Booleans
+//I am using slice to extract a range of elements within my array.
+console.log(ages.slice(2, 5))
+
+//Array of Booleans
 var bools = [true, false, false, true]
+console.log(bools)
+
+//I am using push to add another value or element at the end of my array.
+console.log(bools.push(true))
+console.log(bools)
