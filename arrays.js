312bf841addbbd470936c01ed29299cadfd5d3da
Jenn Yacoubian <jyacoubian@gmail.com>
2022-06-08 14:04:14 -0600

    Declare variables in array

---
+++ arrays.js
@@ -0,0 +1,8 @@
+//#Array of Strings
+var friends = ["Karin", "Katie", "Rachel", "Camay", "Brandon"]
+
+//#Array of Numbers
+var ages = [12, 13, 14, 15, 16]
+
+//#Array of Booleans
+var bools = [true, false, false, true]
