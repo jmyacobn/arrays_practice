b4242c900e6bf38ca6e48eb5c6e05e32103fda4d
Jenn Yacoubian <jyacoubian@gmail.com>
2022-06-08 14:33:04 -0600

    Index position summary

--- arrays.js
+++ arrays.js
@@ -22,3 +22,11 @@ console.log(bools)
 //I am using push to add another value or element at the end of my array.
 console.log(bools.push(true))
 console.log(bools)
+
+//Index positions
+//Each of the arrays above has elements.
+//The position of the element within in the array is called its index.
+//The first element has an index position of 0.
+//The index position of "Rachel" in the friends array is 2.
+//The index position of 16 in the ages array is 4.
+//The index position of the first false in the bools array is 1.
